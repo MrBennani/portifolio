9f8ec2c5cfd9fdfa170dc65cd477f233dfbab0e3
CapyvaraPaulista <victorbonini.vb@gmail.com>
2026-04-09 16:32:31 -0300

    updating infos

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -9,8 +9,6 @@ const Sobre = () => (
             Olá, Eu sou Mr. Bennani, ou Victor Bonini um jovem aspirante a desenvolvidor, e este é meu portifolio com meus projetos que estou sempre atualizando conforme minhas habilidades melhoram. Sinta se livre para olhar meus projetos ou entrar em contato pelo meu LinkPage.
         </Paragrafo>
         <Githubsecao>
-            <img src="https://github-readme-stats.vercel.app/api?username=CapyvaraPaulista&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
-            <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=CapyvaraPaulista&layout=compact&langs_count=7&theme=dracula"/>
         </Githubsecao>
     </section>
 )
